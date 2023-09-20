2c044242a802896821094b372e870c4460d56c35
Lucas Vidal <luccas.lima12@gmail.com>
2023-09-20 14:38:17 -0300

    Resolvendo o problema de transição das imagens quando mudava de tema

--- js/modules/dark-mode.js
+++ js/modules/dark-mode.js
@@ -9,17 +9,17 @@ export default function initDarkMode() {
       document.querySelector("#email").src = "./img/redes/email-mostarda.svg";
       document.querySelector("#github").src = "./img/redes/github-mostarda.svg";
       document.querySelector("#instagram").src =
-        "/img/redes/instagram-mostarda.svg";
+        "./img/redes/instagram-mostarda.svg";
       document.querySelector("#linkedin").src =
-        "/img/redes/linkedin-mostarda.svg";
+        "./img/redes/linkedin-mostarda.svg";
     } else {
       document.querySelector("#logo").src = "./img/logo/logo-amarelo.svg";
       document.querySelector("#email").src = "./img/redes/email-amarelo.svg";
       document.querySelector("#github").src = "./img/redes/github-amarelo.svg";
       document.querySelector("#instagram").src =
-        "/img/redes/instagram-amarelo.svg";
+        "./img/redes/instagram-amarelo.svg";
       document.querySelector("#linkedin").src =
-        "/img/redes/linkedin-amarelo.svg";
+        "./img/redes/linkedin-amarelo.svg";
     }
   });
 }

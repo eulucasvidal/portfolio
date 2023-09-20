0bdfa4d7acc1b902a59943ed9ba64362f8c26585
Lucas Vidal <luccas.lima12@gmail.com>
2023-09-20 14:23:49 -0300

    Adicionando funcionalidade de troca de imagens de SVG ao alterar o tema para melhor visibilidade

--- js/modules/dark-mode.js
+++ js/modules/dark-mode.js
@@ -1,7 +1,25 @@
 export default function initDarkMode() {
-const btnToggleMode = document.querySelector("#btn-dark-mode");
+  const btnToggleMode = document.querySelector("#btn-dark-mode");
 
-btnToggleMode.addEventListener("change", (event) => {
-  document.body.classList.toggle("dark");
-});
+  btnToggleMode.addEventListener("change", (event) => {
+    let modoDarkOn = document.body.classList.toggle("dark");
+
+    if (modoDarkOn) {
+      document.querySelector("#logo").src = "./img/logo/logo-mostarda.svg";
+      document.querySelector("#email").src = "./img/redes/email-mostarda.svg";
+      document.querySelector("#github").src = "./img/redes/github-mostarda.svg";
+      document.querySelector("#instagram").src =
+        "/img/redes/instagram-mostarda.svg";
+      document.querySelector("#linkedin").src =
+        "/img/redes/linkedin-mostarda.svg";
+    } else {
+      document.querySelector("#logo").src = "./img/logo/logo-amarelo.svg";
+      document.querySelector("#email").src = "./img/redes/email-amarelo.svg";
+      document.querySelector("#github").src = "./img/redes/github-amarelo.svg";
+      document.querySelector("#instagram").src =
+        "/img/redes/instagram-amarelo.svg";
+      document.querySelector("#linkedin").src =
+        "/img/redes/linkedin-amarelo.svg";
+    }
+  });
 }

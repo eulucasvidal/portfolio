0906dfebeb34239193c74a26f9f7a3385729883e
Lucas Vidal <luccas.lima12@gmail.com>
2023-09-26 23:58:21 -0300

    Alinhando cores da seção formação com o modo dark

--- js/modules/dark-mode.js
+++ js/modules/dark-mode.js
@@ -8,18 +8,22 @@ export default function initDarkMode() {
       document.querySelector("#logo").src = "./img/logo/logo-mostarda.svg";
       document.querySelector("#email").src = "./img/redes/email-mostarda.svg";
       document.querySelector("#github").src = "./img/redes/github-mostarda.svg";
-      document.querySelector("#instagram").src =
-        "./img/redes/instagram-mostarda.svg";
-      document.querySelector("#linkedin").src =
-        "./img/redes/linkedin-mostarda.svg";
+      document.querySelector("#instagram").src ="./img/redes/instagram-mostarda.svg";
+      document.querySelector("#linkedin").src ="./img/redes/linkedin-mostarda.svg";
+
+      document.querySelector("#faculdade").src ="./img/icones/faculdade-mostarda.svg";
+      document.querySelector("#cursos").src ="./img/icones/cursos-mostarda.svg";
+      document.querySelector("#idioma").src ="./img/icones/idioma-mostarda.svg";
     } else {
       document.querySelector("#logo").src = "./img/logo/logo-amarelo.svg";
       document.querySelector("#email").src = "./img/redes/email-amarelo.svg";
       document.querySelector("#github").src = "./img/redes/github-amarelo.svg";
-      document.querySelector("#instagram").src =
-        "./img/redes/instagram-amarelo.svg";
-      document.querySelector("#linkedin").src =
-        "./img/redes/linkedin-amarelo.svg";
+      document.querySelector("#instagram").src ="./img/redes/instagram-amarelo.svg";
+      document.querySelector("#linkedin").src ="./img/redes/linkedin-amarelo.svg";
+
+      document.querySelector("#faculdade").src ="./img/icones/faculdade-amarelo.svg";
+      document.querySelector("#cursos").src ="./img/icones/cursos-amarelo.svg";
+      document.querySelector("#idioma").src ="./img/icones/idioma-amarelo.svg";
     }
   });
 }

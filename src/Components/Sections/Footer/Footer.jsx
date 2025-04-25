e5410d5a97a84a20c62f0650f488e667791c62bd
Lucas Vidal <luccas.lima12@gmail.com>
2025-04-25 10:49:54 -0300

    Adicionando funcionalidades para mobile

--- src/Components/Sections/Footer/Footer.jsx
+++ src/Components/Sections/Footer/Footer.jsx
@@ -8,37 +8,39 @@ const Footer = () => {
     <footer className={`${styles.footerBg}`}>
       <section className={`${styles.footer} container`}>
         <Subtitle text="vamos trabalhar juntos?" />
-        <div className={`${styles.footerMenu} links`}>
-          <ul>
-            <li>
-              <a href="#">home</a>
-            </li>
-            <li>
-              <a href="#">sobre</a>
-            </li>
-            <li>
-              <a href="#">projetos</a>
-            </li>
-            <li>
-              <a href="#">habilidades</a>
-            </li>
-            <li>
-              <a href="#">contato</a>
-            </li>
-          </ul>
-        </div>
-        <div className={`${styles.footerMenu} links`}>
-          <ul>
-            <li>
-              <a href="https://www.instagram.com/eu.lucasvidal/" target='_blank' rel='noopener noreferrer'>instagram</a>
-            </li>
-            <li>
-              <a href="https://github.com/eulucasvidal" target='_blank' rel='noopener noreferrer'>gitHub</a>
-            </li>
-            <li>
-              <a href="https://www.linkedin.com/in/goncalves-lucas/" target='_blank' rel='noopener noreferrer'>linkedin</a>
-            </li>
-          </ul>
+        <div className={`${styles.footerContainer}`}>
+          <div className={`${styles.footerMenu} links`}>
+            <ul>
+              <li>
+                <a href="#">home</a>
+              </li>
+              <li>
+                <a href="#">sobre</a>
+              </li>
+              <li>
+                <a href="#">projetos</a>
+              </li>
+              <li>
+                <a href="#">habilidades</a>
+              </li>
+              <li>
+                <a href="#">contato</a>
+              </li>
+            </ul>
+          </div>
+          <div className={`${styles.footerMenu} links`}>
+            <ul>
+              <li>
+                <a href="https://www.instagram.com/eu.lucasvidal/" target='_blank' rel='noopener noreferrer'>instagram</a>
+              </li>
+              <li>
+                <a href="https://github.com/eulucasvidal" target='_blank' rel='noopener noreferrer'>gitHub</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/goncalves-lucas/" target='_blank' rel='noopener noreferrer'>linkedin</a>
+              </li>
+            </ul>
+          </div>
         </div>
       </section>
       <div className={`${styles.license}`}>
